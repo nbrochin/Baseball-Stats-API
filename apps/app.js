890f55b0d1fe2f99ce9da84e6d331501391b56aa
Nate Brochin <nbrochin@gmail.com>
2016-05-07 04:00:07 -0400

    ere it is

--- apps/app.js
+++ apps/app.js
@@ -55,10 +55,10 @@ $("#first-choice").change(function() {
         vals = ['Please choose from above'];
     }
     
-    var $secondChoice = $("#second-choice ul.dropdownhidden_demo2");
+    var $secondChoice = $("#second-choice ul.dropdownhidden_demo2 li");
     $secondChoice.empty();
     $.each(vals, function(index, value) {
-      $secondChoice.append("<li><a>" + value + "<a></li>");
+      $secondChoice.append("<li>" + value + "</li>");
     });
 
   });
