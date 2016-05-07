6faa68f4754a38672c364ad4a5658b352af9b4d0
Nate Brochin <nbrochin@gmail.com>
2016-05-07 03:43:32 -0400

    working api

--- apps/app.js
+++ apps/app.js
@@ -1,57 +1,74 @@
 $(document).ready(function(){
 
-$(function() {
-        var params = {
-        key: "bb32e0f1b71046f4a5844dc55af9e844",
-        // teams: "ARI",
-        // callback: "callback"
-            // Request parameters
-        };
+// $(function() {
+//         var params = {
+//         key: "bb32e0f1b71046f4a5844dc55af9e844",
+
+//         };
       
-        $.ajax({
-            url: "https://api.fantasydata.net/mlb/v2/json/teams?" + $.param(params),
-            
-            type: "GET",
-            // Request body
-            // data: 'Division',
-        })
-        .done(function(data) {
-            alert("success");
-        })
-        .fail(function() {
-            alert("error");
-        });
+//         $.ajax({
+//             url: "https://api.fantasydata.net/mlb/v2/JSON/Players/PIT?" + $.param(params),
+//             dataType: "jsonp",
+//             type: "GET",
+          
+//         })
+
+//         .success(function(data) {
+//             // alert("success")
+//             var Name = "";
+          
+//           $.each(data, function(i, data) {
+//               var teamCity = data.City;
+//               // console.log(data.Position + ": " + data.FirstName + " " + data.LastName + ",");
+
+//               Name += 'eggs';
+              
+//               $('#QA-holder').append(teamCity + " " + data.Name + ", ");
+//           })
+
+         
+
+//         // .fail(function() {
+//         //     alert("error");
+//         // });
+//         });
+       
+//     });
+
+$("#first-choice").change(function() {
+
+  var $dropdown = $(this);
+
+  $.getJSON("data.json", function(data) {
+    console.log(data)
+    var key = $dropdown.val();
+    var vals = [];
+              
+    switch(key) {
+      case 'orioles':
+        vals = data.orioles.split(",");
+        break;
+      case 'pirates':
+        vals = data.pirates.split(",");
+        break;
+      case 'base':
+        vals = ['Please choose from above'];
+    }
+    
+    var $secondChoice = $("#second-choice");
+    $secondChoice.empty();
+    $.each(vals, function(index, value) {
+      $secondChoice.append("<option>" + value + "</option>");
     });
 
+  });
+});
+
+  });
 
     //-------------------------------------- end fantasydata code --------------------
 
 
 
-  // }); 
-// FantasyData.mlb = {};
-
-//   FantasyData.mlb.activeTeams = function(callback) {
-//     var uri = buildMlbUrl('Teams');
-
-//     makeRequest(uri, callback);
-//   };
 
 
-
-// function showResults(results){
-//   $.each(results, function(index,fddata){
-//   	  var Height = data.Player.Height;
-//   	  var LastName = data.Player.LastName;
-
-
-//     // console.log(video.snippet.thumbnails.medium.url);
-//     // console.log(ytdata.nextPageToken);
-//     // console.log(video.snippet.vTitle);
-//   });
-
-//   $('#search-results').html(LastName);
-
-// }
-
-});
