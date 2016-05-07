981fbba3291ca22b63f634e228d711d05766360f
Nate Brochin <nbrochin@gmail.com>
2016-05-07 03:56:07 -0400

    dropdownhidden_demo2

--- apps/app.js
+++ apps/app.js
@@ -55,10 +55,10 @@ $("#first-choice").change(function() {
         vals = ['Please choose from above'];
     }
     
-    var $secondChoice = $("#second-choice");
+    var $secondChoice = $("#second-choice ul.dropdownhidden_demo2");
     $secondChoice.empty();
     $.each(vals, function(index, value) {
-      $secondChoice.append("<li>" + value + "</li>");
+      $secondChoice.append("<li><a>" + value + "<a></li>");
     });
 
   });
