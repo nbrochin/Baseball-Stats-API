fd76e6e574aa99282292f16c551802d352c50e07
Nate Brochin <nbrochin@gmail.com>
2016-05-07 18:53:56 -0400

    all players

--- apps/app.js
+++ apps/app.js
@@ -1,55 +1,138 @@
 $(document).ready(function(){
 
-// $(function() {
-//         var params = {
-//         key: "bb32e0f1b71046f4a5844dc55af9e844",
+$(function() {
+        var params = {
+        key: "bb32e0f1b71046f4a5844dc55af9e844",
 
-//         };
+        };
       
-//         $.ajax({
-//             url: "https://api.fantasydata.net/mlb/v2/JSON/Players/PIT?" + $.param(params),
-//             dataType: "jsonp",
-//             type: "GET",
+        $.ajax({
+            url: "https://api.fantasydata.net/mlb/v2/JSON/Players/OAK?" + $.param(params),
+            dataType: "jsonp",
+            type: "GET",
           
-//         })
+        })
 
-//         .success(function(data) {
-//             // alert("success")
-//             var Name = "";
+        .success(function(data) {
+            alert("success")
+            var Name = "";
+            var City = "";
           
-//           $.each(data, function(i, data) {
-//               var teamCity = data.City;
-//               // console.log(data.Position + ": " + data.FirstName + " " + data.LastName + ",");
+          $.each(data, function(i, data) {
+              var teamCity = data.City;
+              console.log(data.Position + ": " + data.FirstName + " " + data.LastName + ",");
 
-//               Name += 'eggs';
+              Name += 'eggs';
+              City += 'tuna';
               
-//               $('#QA-holder').append(teamCity + " " + data.Name + ", ");
-//           })
+              // $('#QA-holder').append(teamCity + " " + data.Division + ", ");
+          })
 
          
 
-//         // .fail(function() {
-//         //     alert("error");
-//         // });
-//         });
+        // .fail(function() {
+        //     alert("error");
+        // });
+        });
        
-//     });
+    });
 
 $("#first-choice").change(function() {
 
   var $dropdown = $(this);
 
-  $.getJSON("data.json", function(data) {
-    console.log(data)
+  $.getJSON("../data.json", function(data) {
+    // console.log(data)
     var key = $dropdown.val();
     var vals = [];
               
     switch(key) {
-      case 'orioles':
+      case 'Braves':
+        vals = data.braves.split(",");
+        break;
+      case 'Orioles':
         vals = data.orioles.split(",");
         break;
-      case 'pirates':
+        case 'Red Sox':
+        vals = data.redSox.split(",");
+        break;
+      case 'Blue Jays':
+        vals = data.blueJays.split(",");
+        break;
+        case 'Rays':
+        vals = data.rays.split(",");
+        break;
+      case 'Cubs':
+        vals = data.cubs.split(",");
+        break;
+        case 'White Sox':
+        vals = data.whiteSox.split(",");
+        break;
+      case 'Reds':
+        vals = data.reds.split(",");
+        break;
+        case 'Indians':
+        vals = data.indians.split(",");
+        break;
+      case 'Rockies':
+        vals = data.rockies.split(",");
+        break;
+        case 'Tigers':
+        vals = data.tigers.split(",");
+        break;
+      case 'Astros':
+        vals = data.astros.split(",");
+        break;
+        case 'Royals':
+        vals = data.royals.split(",");
+        break;
+      case 'Angels':
+        vals = data.angels.split(",");
+        break;
+        case 'Dodgers':
+        vals = data.Dodgers.split(",");
+        break;
+      case 'Marlins':
+        vals = data.marlins.split(",");
+        break;
+        case 'Brewers':
+        vals = data.brewers.split(",");
+        break;
+      case 'Twins':
+        vals = data.twins.split(",");
+        break;
+        case 'Mets':
+        vals = data.mets.split(",");
+        break;
+      case 'Yankees':
+        vals = data.yankees.split(",");
+        break;
+        case 'Athletics':
+        vals = data.athletics.split(",");
+        break;
+        case 'Phillies':
+        vals = data.phillies.split(",");
+        break;
+        case 'Pirates':
         vals = data.pirates.split(",");
+        break;
+        case 'Padres':
+        vals = data.padres.split(",");
+        break;
+        case 'Mariners':
+        vals = data.mariners.split(",");
+        break;
+        case 'Giants':
+        vals = data.giants.split(",");
+        break;
+        case 'Cardinals':
+        vals = data.cardinals.split(",");
+        break;
+        case 'Rangers':
+        vals = data.rangers.split(",");
+        break;
+        case 'Nationals':
+        vals = data.nationals.split(",");
         break;
       case 'base':
         vals = ['Please choose from above'];
