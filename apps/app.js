ad0307a593811b9b51e9d3c544bfa0972fc7a40a
Nate Brochin <nbrochin@gmail.com>
2016-05-09 20:50:30 -0400

    no json file

--- apps/app.js
+++ apps/app.js
@@ -5,7 +5,7 @@ $("#first-choice").change(function() {
     var $dropdown = $(this);
     var trigger = $dropdown.val();
     var vals = [];
-    var oValue = $('#first-choice option:selected').text();
+    var oValue = $('#first-choice').val();
     console.log(oValue);
 
 $(function() {
@@ -15,12 +15,49 @@ $(function() {
         };
       
         $.ajax({
-            url: "https://api.fantasydata.net/mlb/v2/JSON/PlayerSeasonStatsByPlayer/2016/10000031?" + $.param(params),
+            url: "https://api.fantasydata.net/mlb/v2/JSON/Players/" + oValue + "?" + $.param(params),
+            dataType: "jsonp",
+            type: "GET",
+
+         })
+
+        .success(function(data) {
+            var key = $dropdown.val();
+            var vals = [];
+            var Name = data.Name;
+            var PID = data.PlayerID;
+            var $secondChoice = $("#second-choice");
+            $secondChoice.empty();
+            $.each(vals, function(index, value) {
+            $secondChoice.append("<option value=" + PID + ">" + Name + "</option>");
+          })
+          })
+});
+
+
+$("#second-choice").change(function() {
+
+    var $dropdown = $(this);
+    var trigger = $dropdown.val();
+    var vals = [];
+    var PID2 = $('#second-choice').val();
+    console.log(PID2);
+
+})
+
+
+$(function() {
+        var params = {
+        key: "bb32e0f1b71046f4a5844dc55af9e844",
+
+        };
+      
+        $.ajax({
+            url: "https://api.fantasydata.net/mlb/v2/JSON/PlayerSeasonStatsByPlayer/2016/" + PID2 + "?" + $.param(params),
             dataType: "jsonp",
             type: "GET",
           
         })
-
 
         .success(function(data) {
             alert("success")
@@ -35,6 +72,10 @@ $(function() {
             var OnBasePlusSlugging = data.OnBasePlusSlugging.toFixed(3);
             var OnBasePercentage = data.OnBasePercentage.toFixed(3);
 
+
+          })
+          
+    });
           
           $.each(data, function(i, data) {
               // console.log(data.PlayerID + ": " + data.FirstName + " " + data.LastName + ",");
@@ -62,120 +103,23 @@ $(function() {
        
 //         // });
        
-    });
+  
 
-$("#first-choice").change(function() {
+// $("#first-choice").change(function() {
 
-  var $dropdown = $(this);
+//   var $dropdown = $(this);
 
-  $.getJSON("data.json", function(data) {
-    var key = $dropdown.val();
-    var vals = [];
-              
-    switch(key) {
-      case 'braves':
-        vals = data.braves.split(",");
-        break;
-      case 'orioles':
-        vals = data.orioles.split(",");
-        break;
-        case 'redSox':
-        vals = data.redSox.split(",");
-        break;
-      case 'blueJays':
-        vals = data.blueJays.split(",");
-        break;
-        case 'rays':
-        vals = data.rays.split(",");
-        break;
-      case 'cubs':
-        vals = data.cubs.split(",");
-        break;
-        case 'whiteSox':
-        vals = data.whiteSox.split(",");
-        break;
-      case 'reds':
-        vals = data.reds.split(",");
-        break;
-        case 'indians':
-        vals = data.indians.split(",");
-        break;
-      case 'rockies':
-        vals = data.rockies.split(",");
-        break;
-        case 'tigers':
-        vals = data.tigers.split(",");
-        break;
-      case 'astros':
-        vals = data.astros.split(",");
-        break;
-        case 'royals':
-        vals = data.royals.split(",");
-        break;
-      case 'angels':
-        vals = data.angels.split(",");
-        break;
-        case 'dodgers':
-        vals = data.dodgers.split(",");
-        break;
-      case 'marlins':
-        vals = data.marlins.split(",");
-        break;
-        case 'brewers':
-        vals = data.brewers.split(",");
-        break;
-      case 'twins':
-        vals = data.twins.split(",");
-        break;
-        case 'mets':
-        vals = data.mets.split(",");
-        break;
-      case 'yankees':
-        vals = data.yankees.split(",");
-        break;
-        case 'athletics':
-        vals = data.athletics.split(",");
-        break;
-        case 'phillies':
-        vals = data.phillies.split(",");
-        break;
-        case 'pirates':
-        vals = data.pirates.split(",");
-        break;
-        case 'padres':
-        vals = data.padres.split(",");
-        break;
-        case 'mariners':
-        vals = data.mariners.split(",");
-        break;
-        case 'giants':
-        vals = data.giants.split(",");
-        break;
-        case 'cardinals':
-        vals = data.cardinals.split(",");
-        break;
-        case 'rangers':
-        vals = data.rangers.split(",");
-        break;
-        case 'nationals':
-        vals = data.nationals.split(",");
-        break;
-      case 'base':
-        vals = ['Please choose from above'];
-    }
-    
-    var $secondChoice = $("#second-choice");
-    $secondChoice.empty();
-    $.each(vals, function(index, value) {
-      $secondChoice.append("<option>" + value + "</option>");
+//   $.ajax({
+//             url: "https://api.fantasydata.net/mlb/v2/JSON/PlayerSeasonStatsByTeam/2016/10000031?" + $.param(params),
+//             dataType: "jsonp",
+//             type: "GET",
+          
+//         })
+
+ 
       // $secondChoice.append("<li><a href= value=" + "'" + value + "'" + ">" + value + "</a></li>");
 
-    });
-
-  });
-});
-
-  });
+  
 
     //-------------------------------------- end fantasydata code --------------------
 
