f3265ea2758fccf261f6f44f6f114c00c8e225b1
Nate Brochin <nbrochin@gmail.com>
2016-05-12 09:43:50 -0400

    images

--- apps/app.js
+++ apps/app.js
@@ -19,6 +19,7 @@ $(function() {
             dataType: "jsonp",
             type: "GET",
 
+
          })
 
         .success(function(data) {
@@ -29,115 +30,133 @@ $(function() {
             var vals = [];
             var Name = data.FirstName + ' ' + data.LastName;
             var PID = data.PlayerID;
+            // var photoURL = data.photoUrl
+            // var salary = data.Salary;
+         
 
-          // switch(key) {
-          //   case 'COL':
-          //   vals = data.PlayerID.split(":");
-          // }
-            
             var $secondChoice = $("#second-choice");
             $secondChoice.empty();  
 
             var newValues = "";
+            // var salaries = "";
             $("#second-choice").append("<option>Please Choose a Player...</option>");
             for (var i = 0; i < data.length; i++) {
               newValues+= $("#second-choice").append("<option value=" + data[i].PlayerID + ">" + data[i].FirstName + " " + data[i].LastName + "</option>");
+              // salary+= $("ul.footer-box").append(data[i].salary);
+
           }
 
-          }) 
-});
 
-
-$("#second-choice").change(function() {
-
-    var $dropdown = $(this);
-    var trigger = $dropdown.val();
-    var vals = [];
-
-})
-
-$('#idGo').click(function playerData () {
-  var PID2 = $('#second-choice').val();
-});
-  
-  PID2 = $('#second-choice').val();
-
-$(function() {
-        var params = {
-        key: "bb32e0f1b71046f4a5844dc55af9e844",
-
-        };
-
-        
-
-        $.ajax({
-            url: "https://api.fantasydata.net/mlb/v2/JSON/PlayerSeasonStatsByPlayer/2016/" + PID2 + "?" + $.param(params),
-            dataType: "jsonp",
-            type: "GET",
-          
         })
+});
 
-        .success(function(data) {
-            alert("success")
+
+            $("#second-choice").change(function() {
+
+                var $dropdown = $(this);
+                var trigger = $dropdown.val();
+                var vals = [];
+                var PID2 = $('#second-choice').val();
+
+           
+
+            // $('#idGo').submit(function(e) {
+            //   e.preventDefault();
+            //   PID2 = $('#second-choice').val();
+            // });
+            
+                var params = {
+                key: "bb32e0f1b71046f4a5844dc55af9e844",
+
+                };
+
+                console.log(PID2);
+                $.ajax({
+                        url: "https://api.fantasydata.net/mlb/v2/JSON/PlayerSeasonStatsByPlayer/2016/" + PID2 + "?" + $.param(params),
+                        dataType: "jsonp",
+                        type: "GET",
+                      
+                })
+          
+              // e.preventDefault();
+  
+
+           .success(function(data) {
+                alert("success")
+
             var Name = data.Name;
             var Team = data.Team;
             var Games = data.Games;
             var AtBats = data.AtBats;
             var Hits = data.Hits;
-            var RunsBattedIn = data.RunsBattedIn;
+            var RBI = data.RunsBattedIn;
             var BattingAverage = data.BattingAverage.toFixed(3);
-            var SluggingPercentage = data.SluggingPercentage.toFixed(3);
-            var OnBasePlusSlugging = data.OnBasePlusSlugging.toFixed(3);
+            var SLG = data.SluggingPercentage.toFixed(3);
+            var OPS = data.OnBasePlusSlugging.toFixed(3);
             var OnBasePercentage = data.OnBasePercentage.toFixed(3);
+            var Wins = data.Wins;
+            var Losses = data.Losses;
+            var IP = data.InningsPitchedDecimal;
+            var ERA = data.EarnedRunAverage.toFixed(2);
+            var pWalks = data.PitchingWalks;
+            var pHits = data.PitchingHits;
+            var BAA = data.PitchingBattingAverageAgainst.toFixed(3);
+      
 
+            if (data.PositionCategory == "P") {
+                console.log(data.PositionCategory);
+                $('table.pitcher-season-stats').show();
+           
+                $('div.yr-selector span.16').css({
+                  color: 'white',
+                  backgroundColor: 'black',
+                  padding: '5px'
+                });
+                $('div.top-data-section span.playerPhoto').html("<img src='http://static.fantasydata.com/headshots/mlb/low-res/" + PID2 + ".png'" + ">");
+                $('div.top-data-section span.playerName').text(Name);
+                $('div.top-data-section span.city-team-name').text(Team);
+                $('td.Games').text(Games);
+                $('td.Wins').text(Wins);
+                $('td.Losses').text(Losses);
+                $('td.IP').text(IP);
+                $('td.ERA').text(ERA);
+                $('td.pWalks').text(pWalks);
+                $('td.pHits').text(pHits);
+                $('td.BAA').text(BAA);
+            
+            } else  {
 
-         
-            console.log(PID2);
-
-          
-          $.each(data, function(i, data) {
-              
-              $('div.top-data-section h2').text(Name);
-              $('div.top-data-section h2 span .city-team-name').text(Team);
-              $('table.season-stats td.Games').text(Games);
-              $('table.season-stats td.AtBats').text(AtBats);
-              $('table.season-stats td.Hits').text(Hits);
-              $('table.season-stats td.RunsBattedIn').text(RunsBattedIn);
-              $('table.season-stats td.BattingAverage').text(BattingAverage);
-              $('table.season-stats td.SluggingPercentage').text(SluggingPercentage);
-              $('table.season-stats td.OnBasePlusSlugging').text(OnBasePlusSlugging);
-              $('table.season-stats td.OnBasePercentage').text(OnBasePercentage);
+                $('table.batter-season-stats').show();
+    
+                $('div.yr-selector span.16').css({
+                  color: 'white',
+                  backgroundColor: 'black',
+                  padding: '5px'
+                });
+                $('div.top-data-section span.playerPhoto').html("<img src='http://static.fantasydata.com/headshots/mlb/low-res/" + PID2 + ".png'" + ">");
+                $('div.top-data-section span.playerName').text(Name);
+                $('div.top-data-section span.city-team-name').text(Team);
+                $('td.Games').text(Games);
+                $('td.AtBats').text(AtBats);
+                $('td.Hits').text(Hits);
+                $('td.RunsBattedIn').text(RBI);
+                $('td.BattingAverage').text(BattingAverage);
+                $('td.SluggingPercentage').text(SLG);
+                $('td.OnBasePlusSlugging').text(OPS);
+                $('td.OnBasePercentage').text(OnBasePercentage);
+            
+            }
           })
           })
-        })
-  });
-  });
+          })
+            // alert("success")
+            // console.log(PID2);
 
-  
-//         // .fail(function() {
-//         //     alert("error");
-       
-//         // });
-       
-  
-
-// $("#first-choice").change(function() {
-
-//   var $dropdown = $(this);
-
-//   $.ajax({
-//             url: "https://api.fantasydata.net/mlb/v2/JSON/PlayerSeasonStatsByTeam/2016/10000031?" + $.param(params),
-//             dataType: "jsonp",
-//             type: "GET",
           
-//         })
-
- 
-      // $secondChoice.append("<li><a href= value=" + "'" + value + "'" + ">" + value + "</a></li>");
+         });
+        
 
   
-
-    //-------------------------------------- end fantasydata code --------------------
 
 
 
