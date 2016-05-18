2395674a46477a7545cad596fbbf728eb403eade
Nate Brochin <nbrochin@gmail.com>
2016-05-18 14:18:45 -0400

    changes

--- apps/app-radar.js
+++ apps/app-radar.js
@@ -14,10 +14,10 @@ $(function getRequest () {
 
         // };
       
-       $.ajax({
+       $.get({
             url: "http://api.sportradar.us/mlb-t5/seasontd/2016/REG/teams/1d678440-b4b1-4954-9b39-70afb3ebbcfa/statistics.json?api_key=pfqnd9eqk9j4jjc7au287rdt",
-            dataType: "jsonp",
-            type: "GET"
+            // dataType: "jsonp",
+            // type: "GET"
             // crossDomain: true,
             // xhrFields: {
             //     withCredentials: true
