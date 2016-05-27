e04811879684907a70de28f35af42a7514a3d1c6
Nate Brochin <nbrochin@gmail.com>
2016-05-27 15:00:19 -0400

    change by year toggle

--- apps/app.js
+++ apps/app.js
@@ -7,7 +7,6 @@ $(document).ready(function(){
         var vals = [];
         var teamId = $('#first-choice').val();
         var teamName = $('#first-choice option:selected').text();
-        console.log(teamId);
 
       
 
@@ -26,28 +25,22 @@ $(document).ready(function(){
                    })
 
                     .success(function(data) {
-                      // alert("success")
-                      // console.log(data[6].PlayerID);
                       // console.log(data[9].FirstName);
-                        var key = $dropdown.val();
-                        var vals = [];
-                        var Name = data.FirstName + ' ' + data.LastName;
-                        var PID = data.PlayerID;
-                        var salary = {};
-                        var jerseyNo = {};
-                        console.log(data[6].Jersey)
-
-                        var $secondChoice = $("#second-choice");
-                        $secondChoice.empty();  
+                        var key = $dropdown.val(),
+                            vals = [],
+                            Name = data.FirstName + ' ' + data.LastName,
+                            PID = data.PlayerID,
+                            salary = {},
+                            jerseyNo = {},
+                            $secondChoice = $("#second-choice");
+                            $secondChoice.empty();  
 
                         var newValues = "";
-                        // var salaries = "";
                         $("#second-choice").append("<option>Please Choose a Player...</option>");
                         for (var i = 0; i < data.length; i++) {
                           salary[data[i].PlayerID] = data[i].Salary;
                           jerseyNo[data[i].PlayerID] = data[i].Jersey;
                           newValues+= $("#second-choice").append("<option value=" + data[i].PlayerID + ">" + data[i].FirstName + " " + data[i].LastName + "</option>");
-                          // salary+= $("ul.footer-box").append(data[i].salary);
 
                       }
 
@@ -65,7 +58,7 @@ $(document).ready(function(){
                 dollars = dollars.split('').reverse().join('')
                     .replace(/(\d{3}(?!$))/g, '$1,')
                     .split('').reverse().join('');
-                return '$' + dollars + '.' + cents.slice(0, 2);
+                    return '$' + dollars + '.' + cents.slice(0, 2);
                 }
 
           
@@ -84,63 +77,62 @@ $(document).ready(function(){
                 })
           
               // e.preventDefault();
-  
+              
 
            .success(function(data) {
 
-            var Name = data.Name, 
+            var q = data.Name, 
                 Team = data.Team,
                 Games = data.Games,
-                AtBats = data.AtBats,
-                Hits = data.Hits,
-                RBI = data.RunsBattedIn,
-                BattingAverage = data.BattingAverage.toFixed(3),
+                AtBats = data.AtBats.toFixed(0),
+                Hits = data.Hits.toFixed(0),
+                HRs = data.HomeRuns.toFixed(0),
+                RBI = data.RunsBattedIn.toFixed(0),
+                BattingAverage = (data.Hits / data.AtBats).toFixed(3),
                 SLG = data.SluggingPercentage.toFixed(3),
                 OPS = data.OnBasePlusSlugging.toFixed(3),
                 OnBasePercentage = data.OnBasePercentage.toFixed(3),
-                Wins = data.Wins,
-                Losses = data.Losses,
+                Wins = data.Wins.toFixed(0),
+                Losses = data.Losses.toFixed(0),
+                Saves = data.Saves.toFixed(0),
                 IP = data.InningsPitchedDecimal,
                 ERA = data.EarnedRunAverage.toFixed(2),
-                pWalks = data.PitchingWalks,
-                pHits = data.PitchingHits,
+                pWalks = data.PitchingWalks.toFixed(0),
+                pHits = data.PitchingHits.toFixed(0),
                 BAA = data.PitchingBattingAverageAgainst.toFixed(3),
                 Salary = salary[PID2],
-                JerseyNumber = jerseyNo[PID2];
+                JerseyNumber = jerseyNo[PID2],
+                Position = data.Position;
+
 
                 function toUSD(number) {
-                    if (number == undefined) {
+                    if (number == undefined | null) {
                       $('div.top-data-section span.playerSalary').text("N/A");
-                    }
-                var number = number.toString(), 
-                dollars = number.split('.')[0], 
-                cents = (number.split('.')[1] || '') +'00';
-                dollars = dollars.split('').reverse().join('')
-                    .replace(/(\d{3}(?!$))/g, '$1,')
-                    .split('').reverse().join('');
-                return '$' + dollars;
+
+                    } else {
+
+                      var number = number.toString(), 
+                          dollars = number.split('.')[0], 
+                          cents = (number.split('.')[1] || '') +'00';
+                          dollars = dollars.split('').reverse().join('').replace(/(\d{3}(?!$))/g, '$1,').split('').reverse().join('');
+                          return '$' + dollars;
+                          }
                 }
 
             if (data.PositionCategory == "P") {
                 // console.log(data.PositionCategory);
                 $('table.pitcher-season-stats').show();
                 $('table.batter-season-stats').hide();
-
-
-           
-                $('div.yr-selector span.16').css({
-                  color: 'white',
-                  backgroundColor: 'black',
-                  padding: '5px'
-                });
                 $('div.top-data-section span.playerPhoto').html("<img style='border: solid 2px #444444; background-color: white; padding: 6px 6px 0px 6px' src='http://static.fantasydata.com/headshots/mlb/low-res/" + PID2 + ".png'" + " alt='" + Name + "'>");
-                $('div.top-data-section span.playerName').text(Name);
+                $('div.top-data-section span.playerName').text(q);
                 $('div.top-data-section span.city-team-name').text(teamName);
                 $('div.top-data-section span.playerNumber').text(JerseyNumber);
+                $('div.top-data-section span.playerPosition').text(Position);
                 $('div.top-data-section span.playerSalary').text(toUSD(Salary));
                 $('td.Games').text(Games);
                 $('td.Wins').text(Wins);
                 $('td.Losses').text(Losses);
+                $('td.Saves').text(Saves);
                 $('td.IP').text(IP);
                 $('td.ERA').text(ERA);
                 $('td.pWalks').text(pWalks);
@@ -151,20 +143,16 @@ $(document).ready(function(){
 
                 $('table.batter-season-stats').show();
                 $('table.pitcher-season-stats').hide();
-    
-                $('div.yr-selector span.16').css({
-                  color: 'white',
-                  backgroundColor: 'black',
-                  padding: '5px'
-                });
                 $('div.top-data-section span.playerPhoto').html("<img src='http://static.fantasydata.com/headshots/mlb/low-res/" + PID2 + ".png'" + ">");
-                $('div.top-data-section span.playerName').text(Name);
-                $('div.top-data-section span.city-team-name').text(Team);
+                $('div.top-data-section span.playerName').text(q);
+                $('div.top-data-section span.city-team-name').text(teamName);
                 $('div.top-data-section span.playerNumber').text(JerseyNumber);
+                $('div.top-data-section span.playerPosition').text(Position);
                 $('div.top-data-section span.playerSalary').text(toUSD(Salary));
                 $('td.Games').text(Games);
                 $('td.AtBats').text(AtBats);
                 $('td.Hits').text(Hits);
+                $('td.HomeRuns').text(HRs);
                 $('td.RunsBattedIn').text(RBI);
                 $('td.BattingAverage').text(BattingAverage);
                 $('td.SluggingPercentage').text(SLG);
@@ -173,30 +161,76 @@ $(document).ready(function(){
 
             
             }
+
+                              $(function(){
+                              
+                                  q = data.Name;
+                                  getRequest(q);
+                                 
+                                
+                              });
+
+
+                              function getRequest(q){
+                                var params = {
+                                  key:'AIzaSyAx-5dsAUcbAFDTm_8AL7gwwq_kh7UVwXY',
+                                  cx: '005664771509859456768:nroszt1ynw4',
+                                  q: q,
+                                  // imgType: 'photo',
+                                  // num: '16',
+                                  // maxResults:15,
+                                  // pageToken:token,
+                                  // imgSize:'medium', // for image size
+                                  // callback: 'hndlr',
+                                  searchType:'image' // compulsory 
+                                  
+                                };
+
+                                  
+                                url = 'https://www.googleapis.com/customsearch/v1';
+
+                                  $.getJSON(url, params, function(data){
+                                  // showResults(response[14].items);
+                                  console.log(data.items);
+                                  var thumbnailLink = {};
+                                  var image = {};
+                                  var items = {};
+                                  var item = data.items;
+                                  var images = "";
+
+                                      
+                                    for (var i = 0; i < item.length; i++) {
+                                
+                                      // in production code, item.htmlTitle should have the HTML entities escaped.
+                                      // $("section#QA-holder").append += "<br>" + response.items[i];
+                                      images += "<span><img src=" + item[i].image.thumbnailLink + "></span>";
+                                      
+                                      
+                                    }
+                                    $("section#image-search-holder").html(images);
+                                });
+                                    
+                                  
+                                };
+
           })
           })
           })
 
         })
-});
+  });
 
 
-                var season = "none";
-                $(".seasonSelect").click(function() {
-                   season = $(this).attr("data-name");
-                   $('div.yr-selector span.seasonSelect').css({
-                      color: 'white',
-                      backgroundColor: 'black',
-                      padding: '3px'
-                   });
-                  // $(".seasonSelect").hide();
+                   var season = "none";
+                   var dataName = "";
+                   $(".seasonSelect").click(function() {
+                      season = $(this).attr("dataName");
+                      $(".seasonSelect").removeClass('active');
+                      $(this).addClass('active');
+
                 });
 
-                // $("#test").click(function() {
-                //    alert(season);
-                // });
-            // alert("success")
-            // console.log(PID2);
+              
 
           
          });
